d2c2627a0f012129c82a223aa00dfbd15f7a56dc
Viacheslav Akulov <sentence@ngs.ru>
2019-11-03 13:24:40 +0700

    Изменяет конфиг

--- config/config.php
+++ config/config.php
@@ -2,7 +2,8 @@
 
 $config = [
     'sitename' => 'Дела в порядке',  //название сайта
-    'timezone' => 'Europe/Moscow',  //временная зона
+    'timezone' => 'UTC',  //временная зона
+    'user_files_path' => 'uploads/files/',  //путь к загружаемым файлам
     'filters' => [  //фильтры задач
         'all' => 'Все задачи',
         'today' => 'Повестка дня',
