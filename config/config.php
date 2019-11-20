243e96139c4e43a1e0c303f5630f42b70107f5e5
Viacheslav Akulov <sentence@ngs.ru>
2019-11-20 13:54:16 +0700

    Добавляет валидацию типа файла при создании задачи

--- config/config.php
+++ config/config.php
@@ -14,7 +14,14 @@ $config = [
         'title_min_length' => 3,  // Минимальное кол-во символов в названиях/заголовках
         'title_max_length' => 100,  // Максимальное кол-во символов в названиях/заголовках
         'user_name_max_length' => 100,  // Максимальное кол-во символов в имени пользователя
-        'max_file_size' => 20,  // Максимальный размер загружаемого файла, МБ
+        'max_file_size' => 2,  // Максимальный размер загружаемого файла, МБ
+        'mime_types' => [  // Допустимые типы файлов
+            'image/jpeg' => 'jpg, jpeg',
+            'image/png' => 'png',
+            'application/pdf' => 'pdf',
+            'application/msword' => 'doc',
+            'application/vnd.ms-excel' => 'xls',
+        ],
         'password_min_length' => 6,  // Минимальная длина пароля
         'password_max_length' => 72,  // Максимальная длина пароля
     ],
